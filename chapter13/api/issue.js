018795fa88302948e2e6b033bcbe47f328539538
SechabaMadyibi <sechabamadyibi1@gmail.com>
2024-02-24 23:34:34 +0200

    update

--- chapter13/api/issue.js
+++ chapter13/api/issue.js
@@ -7,9 +7,11 @@ async function get(_, { id }) {
     return issue;
    }
 
+
+   const PAGE_SIZE = 10;
 //function returning issuedb array
 //chapter 10- add parameters (effortmin & effortmax) 
-async function list(_, { status, effortMin, effortMax}) {
+async function list(_, { status, effortMin, effortMax, page, }) {
     const db = getDb();
     const filter = {};
  if (status) filter.status = status;
@@ -20,8 +22,15 @@ async function list(_, { status, effortMin, effortMax}) {
     if (effortMin !== undefined) filter.effort.$gte = effortMin;
     if (effortMax !== undefined) filter.effort.$lte = effortMax;
     }
-    const issues = await db.collection('issues').find(filter).toArray();
-    return issues;
+    //get 10 results for each page
+    const cursor = db.collection('issues').find(filter)
+    .sort({ id: 1 })
+    .skip(PAGE_SIZE * (page - 1))
+    .limit(PAGE_SIZE);
+    const totalCount = await cursor.count(false);
+    const issues = cursor.toArray();
+    const pages = Math.ceil(totalCount / PAGE_SIZE);
+    return { issues, pages };
 };
 
 
@@ -83,4 +92,48 @@ async function update(_, { id, changes }) {
     return false;
   }
 
-module.exports = { list, add, get, update, delete: remove, };
+  //method for restoring deleted issue
+  async function restore(_, { id }) {
+    const db = getDb();
+    const issue = await db.collection('deleted_issues').findOne({ id });
+    if (!issue) return false;
+    issue.deleted = new Date();
+    let result = await db.collection('issues').insertOne(issue);
+    if (result.insertedId) {
+    result = await db.collection('deleted_issues').removeOne({ id });
+    return result.deletedCount === 1;
+    }
+    return false;
+   }
+
+  //issue counts api
+  async function counts(_, { status, effortMin, effortMax }) {
+    const db = getDb();
+    const filter = {};
+    if (status) filter.status = status;
+    if (effortMin !== undefined || effortMax !== undefined) {
+    filter.effort = {};
+    if (effortMin !== undefined) filter.effort.$gte = effortMin;
+    if (effortMax !== undefined) filter.effort.$lte = effortMax;
+    }
+    const results = await db.collection('issues').aggregate([
+    { $match: filter },
+    {
+    $group: {
+    _id: { owner: '$owner', status: '$status' },
+    count: { $sum: 1 },
+    },
+    },
+    ]).toArray();
+
+    const stats = {};
+    results.forEach((result) => {
+    // eslint-disable-next-line no-underscore-dangle
+    const { owner, status: statusKey } = result._id;
+    if (!stats[owner]) stats[owner] = { owner };
+    stats[owner][statusKey] = result.count;
+    });
+    return Object.values(stats);
+   }
+
+module.exports = { list, add, get, update, delete: remove, restore, counts, };
